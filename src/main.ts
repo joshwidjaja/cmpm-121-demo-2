94eb1e61c17ed4a0c1c4947f8767f1b6cebb13b2
Joshua Widjaja <josh@widjaja.us>
2023-10-14 12:42:19 -0700

    Step 1 Complete

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,16 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "My game";
+const gameName = "Sticker Sketchpad";
 
 document.title = gameName;
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+const canvas = document.createElement("canvas");
+canvas.width = 256;
+canvas.height = 256;
+//const ctx = canvas.getContext("2d");
+app.append(canvas);
