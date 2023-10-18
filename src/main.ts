c60f6e296430537bd94641f34236c3278bb33d4c
Joshua Widjaja <josh@widjaja.us>
2023-10-17 22:29:03 -0700

    Step 7 Complete

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,7 @@ interface Point {
 let thickness = 1;
 const thinWidth = 1;
 const thickWidth = 4;
+let utensil = "🖊️";
 class LineCommand {
   points: Point[];
 
@@ -38,6 +39,20 @@ class LineCommand {
   }
 }
 
+class ToolCommand {
+  x: number;
+  y: number;
+
+  constructor(x: number, y: number) {
+    this.x = x;
+    this.y = y;
+  }
+  execute() {
+    context.font = "32px monospace";
+    context.fillText(utensil, this.x, this.y);
+  }
+}
+
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
@@ -49,16 +64,21 @@ const context: CanvasRenderingContext2D = canvas.getContext("2d")!;
 app.append(canvas);
 
 const drawingChanged = new Event("drawing-changed");
+const toolChanged = new Event("tool-changed");
 canvas.addEventListener("drawing-changed", () => redraw);
+canvas.addEventListener("tool-changed", () => redraw);
 
 const commands: LineCommand[] = [];
 const redoCommands: LineCommand[] = [];
 let currentLineCommand: LineCommand | null = null;
+let toolCommand: ToolCommand | null = null;
 
 function redraw() {
   context.clearRect(0, 0, canvas.width, canvas.height);
   commands.forEach((cmd) => cmd.display(context));
   //if (currentLineCommand) currentLineCommand.display(context);
+
+  if (toolCommand) toolCommand.execute();
 }
 
 function tick() {
@@ -67,26 +87,44 @@ function tick() {
 }
 tick();
 
+canvas.addEventListener("mouseout", () => {
+  toolCommand = null;
+  canvas.dispatchEvent(toolChanged);
+});
+
+canvas.addEventListener("mouseenter", (event) => {
+  toolCommand = new ToolCommand(event.offsetX, event.offsetY);
+  canvas.dispatchEvent(toolChanged);
+});
+
 // https://shoddy-paint.glitch.me/paint0.html
 canvas.addEventListener("mousedown", (event) => {
+  toolCommand = null;
   currentLineCommand = new LineCommand(event.offsetX, event.offsetY);
   commands.push(currentLineCommand);
   redoCommands.splice(0, redoCommands.length);
 
   canvas.dispatchEvent(drawingChanged);
+  canvas.dispatchEvent(toolChanged);
 });
 
 canvas.addEventListener("mousemove", (event) => {
+  toolCommand = new ToolCommand(event.offsetX, event.offsetY);
   // if cursor/left click is active
   if (event.buttons == 1) {
+    toolCommand = null;
     currentLineCommand!.points.push({ x: event.offsetX, y: event.offsetY });
     canvas.dispatchEvent(drawingChanged);
   }
+
+  canvas.dispatchEvent(toolChanged);
 });
 
-window.addEventListener("mouseup", () => {
+window.addEventListener("mouseup", (event) => {
   currentLineCommand = null;
+  toolCommand = new ToolCommand(event.offsetX, event.offsetY);
   canvas.dispatchEvent(drawingChanged);
+  canvas.dispatchEvent(toolChanged);
 });
 
 app.append(document.createElement("br"));
@@ -96,6 +134,7 @@ app.append(thin);
 
 thin.addEventListener("click", () => {
   thickness = thinWidth;
+  utensil = "🖊️";
   canvas.dispatchEvent(drawingChanged);
 });
 
@@ -105,6 +144,7 @@ app.append(thick);
 
 thick.addEventListener("click", () => {
   thickness = thickWidth;
+  utensil = "🖌️";
   canvas.dispatchEvent(drawingChanged);
 });
 
